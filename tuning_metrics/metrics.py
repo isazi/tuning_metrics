343916f5e42b425bba39789e29bd17e747ac022c
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-05-07 14:30:39 +0200

    Tuning metrics.

---
+++ tuning_metrics/metrics.py
@@ -0,0 +1,23 @@
+
+
+import math
+
+
+def tuning_score_distribution(performance_space, nbins=10):
+    """
+    Computes TS_d, the tuning score based on the distribution of configurations in the performance space.
+    """
+
+    histogram = performance_space.histogram(nbins)
+    if (histogram[0][9] / performance_space.size()) >= 0.1:
+        return ((0.5 / 0.9) * (histogram[0][9] / performance_space.size())) + 1 - (0.5 / 0.9)
+    else:
+        return (0.5 / 0.1) * (histogram[0][9] / performance_space.size())
+
+
+def tuning_score_size(performance_space, peak):
+    """
+    Computes TS_s, the tuning score based on the size of the performance space compared to the achievable performance.
+    """
+
+    return math.sqrt(performance_space.optimum() / peak)
