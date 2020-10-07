0020be4d9e2420cf68b57e1d1d8285a09e5e61c6
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2020-10-07 10:46:15 +0200

    Added type hints and parameter to transform loaded values.

--- tuning_metrics/utils.py
+++ tuning_metrics/utils.py
@@ -1,9 +1,11 @@
 
+from collections.abc import Callable
 import json
 import tuning_metrics.space
 
 
-def load_kernel_tuner(filename, key="time", ascending_metric=False, name=""):
+def load_kernel_tuner(filename: str, key: str, ascending_metric: bool, name: str, transform: Callable) \
+        -> tuning_metrics.space.PerformanceSpace:
     """
     Load JSON files produced by kernel_tuner.
 
@@ -11,12 +13,24 @@ def load_kernel_tuner(filename, key="time", ascending_metric=False, name=""):
     :param key: The key of the metric used for performance results; the default is "time".
     :param ascending_metric: True if the metric is ascending, False otherwise; the default is False.
     :param name: The name of the performance space; the default is "".
-    :return: A PerformanceSpace object.
+    :param transform: Function to be applied to all values loaded from file.
+    :return: A PerformanceSpace object containing the specified metric.
     """
 
+    if not key:
+        key = "time"
+    if not ascending_metric:
+        ascending_metric = False
+    if not name:
+        name = ""
+    if not transform:
+        transform = None
     with open(filename, "r") as file:
         configurations = json.load(file)
     performance_space = tuning_metrics.space.PerformanceSpace(ascending_metric=ascending_metric, name=name)
     for configuration in configurations:
-        performance_space.add_element(configuration[key])
+        if transform is not None:
+            performance_space.add_element(transform(configuration[key]))
+        else:
+            performance_space.add_element(configuration[key])
     return performance_space
